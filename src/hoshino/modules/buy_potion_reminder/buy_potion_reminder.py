6857ca499e51d089323927ef162fc3d766656db5
wosiwq <z_weipeng@126.com>
2020-12-05 16:31:33 +0800

    test

--- src/hoshino/modules/buy_potion_reminder/buy_potion_reminder.py
+++ src/hoshino/modules/buy_potion_reminder/buy_potion_reminder.py
@@ -1,4 +1,5 @@
 import pytz
+import random
 from datetime import datetime
 from hoshino import Service, R
 
@@ -7,6 +8,6 @@ sv = Service('buy_potion_reminder', enable_on_default=False, help_='买药提醒
 
 @sv.scheduled_job('cron', hour='*/6')
 async def hour_call():
-    pic = R.img("BuyPotion.jpg").cqcode
+    pic = R.img(f"BuyPotion{random.randint(1, 3)}.jpg").cqcode
     msg = f'骑士君，该上线买经验药水啦~\n{pic}'
     await sv.broadcast(msg, 'buy_potion_reminder')
